58a2485b579b1dce198a0c9096aa2975ce77050d
zhenduan <zhen.duan.nfd@gmail.com>
2019-10-23 19:34:19 -0700

    Added constrains, now friendly to all screen sizes.

--- Tuta/ViewController.swift
+++ Tuta/ViewController.swift
@@ -14,10 +14,16 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var introTextLabel: UILabel!
     
+    @IBOutlet weak var haveAccountLabel: UILabel!
+    
+    @IBOutlet weak var signUpButton: UIButton!
+    
     var signUpViewController: SignUpViewController?
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setUpContrains()
         // Do any additional setup after loading the view.
         
         
@@ -25,9 +31,28 @@ class ViewController: UIViewController {
     
     @IBAction func signUpButtonPressed(_ sender: UIButton) {
         print("Sign Up button pressed!")
-        introTextLabel.text = "Yes! we are the best!"
+        //introTextLabel.text = "Yes! we are the best!"
     }
     
+    @IBAction func logInButtonPressed(_ sender: UIButton) {
+        print("Log In button pressed!")
+    }
+    
+    func setUpContrains() {
+        // logo origin
+        logoImageView.frame.origin.x = (self.view.frame.width - logoImageView.frame.width) / 2
+        logoImageView.frame.origin.y = self.view.frame.height * 0.40 - logoImageView.frame.height / 2
+        
+        // logo text
+        introTextLabel.frame.origin.x = logoImageView.frame.origin.x + logoImageView.frame.width - introTextLabel.frame.width
+        introTextLabel.frame.origin.y = logoImageView.frame.origin.y + 160
+        
+        // have an account label
+        
+        
+        
+        
+    }
 
 
 }
