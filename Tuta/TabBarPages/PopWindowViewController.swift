1026e5f6a81c6892b9fcaac8623da82e6c8ce79a
zhenduan <zhen.duan.nfd@gmail.com>
2019-11-29 16:43:56 -0800

    Add tap gesture for AddPostcard Page and AddCourse page

--- Tuta/TabBarPages/PopWindowViewController.swift
+++ Tuta/TabBarPages/PopWindowViewController.swift
@@ -24,11 +24,25 @@ class PopWindowViewController: UIViewController{
     let uid = Auth.auth().currentUser?.uid
     var user = TutaUser()
 
-    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        registerForKeyboardNotifications()
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         confirmButton.applyButton()
+        setUpTapGesture()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        deregisterForKeyboardNorifications()
+    }
+    
+    func setUpTapGesture() {
+        let tap = UITapGestureRecognizer(target: self, action:#selector(dismissKeyboard))
+        self.view.addGestureRecognizer(tap)
     }
     
     func showToast(message : String, font: UIFont) {
@@ -53,6 +67,40 @@ class PopWindowViewController: UIViewController{
         }, completion: {(isCompleted) in
             toastLabel.removeFromSuperview()
         })
+    }
+    
+    func registerForKeyboardNotifications() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(notification:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(notification:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    @objc func dismissKeyboard() {
+        self.view.endEditing(true)
+    }
+    
+    @objc func keyboardWillShow(notification: Notification) {
+//        if let keyboardFrame : NSValue = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue {
+//            let keyboardRectangle = keyboardFrame.cgRectValue
+//            let keyboardY = keyboardRectangle.origin.y
+//
+//            if activeField != nil {
+//                distance = keyboardY - (activeField?.frame.origin.y)! - (activeField?.frame.height)! * 2 - self.view.frame.origin.y
+//                //print(distance!)
+//                if (distance.isLess(than: CGFloat.zero)) {
+//                    self.view.frame.origin.y += distance
+//                    //print("screen moved")
+//                }
+//            }
+//        }
+    }
+
+    @objc func keyboardWillHide(notification: Notification) {
+        self.view.frame.origin.y = 0
+    }
+
+    func deregisterForKeyboardNorifications() {
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
     }
     
     
