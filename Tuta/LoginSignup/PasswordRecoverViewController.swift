5a67c80db47b8e176e0d9647c414947b78db9fd7
Alex Lee <yul801@ucsd.edu>
2019-11-30 00:38:11 -0800

    password recovery

---
+++ Tuta/LoginSignup/PasswordRecoverViewController.swift
@@ -0,0 +1,9 @@
+//
+//  PasswordRecoverViewController.swift
+//  Tuta
+//
+//  Created by Alex Li on 11/29/19.
+//  Copyright © 2019 Zhen Duan. All rights reserved.
+//
+
+import Foundation
---
+++ Tuta/LoginSignup/RecoverPasswordViewController.swift
@@ -0,0 +1,43 @@
+//
+//  RecoverPasswordViewController.swift
+//  Tuta
+//
+//  Created by Alex Li on 11/29/19.
+//  Copyright © 2019 Zhen Duan. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import FirebaseAuth
+
+
+class RecoverPasswordViewController: UIViewController{
+    
+    
+    @IBOutlet weak var userEmailTextField: UITextField!
+    override public func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func RecoverPasswordTapped(_ sender: Any) {
+        let userEmail = self.userEmailTextField.text
+        
+        Auth.auth().sendPasswordReset(withEmail: userEmail!, completion: { (error) in
+            if error != nil{
+                let resetFailedAlert = UIAlertController(title: "Reset Failed", message: "Error: \(String(describing: error?.localizedDescription))", preferredStyle: .alert)
+                resetFailedAlert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+                self.present(resetFailedAlert, animated: true, completion: nil)
+            }else {
+                let resetEmailSentAlert = UIAlertController(title: "Reset email sent successfully",message: "Check your email", preferredStyle: .alert)
+                resetEmailSentAlert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+                self.present(resetEmailSentAlert, animated: true, completion: nil)
+            }
+        })
+    }
+        //PRESENT ALERT
+//        self.present(forgotPasswordAlert, animated: true, completion: nil)
+        
+    
+    
+    
+}
